eda6fa16e6136149677f327d1ed859c063c4333b
Dani L <danilopezf10@gmail.com>
2024-09-25 23:42:35 +0200

    Lighter theme, lower case exp items

--- src/app/components/education-section/education-section.component.ts
+++ src/app/components/education-section/education-section.component.ts
@@ -18,10 +18,10 @@ export class EducationSectionComponent {
   @ViewChildren('title, cards') els!: QueryList<ElementRef>;
 
   events = [
-    { status: 'Ordered', date: '15/10/2020', icon: 'pi pi-shopping-cart', color: '#9C27B0', image: 'game-controller.jpg' },
-    { status: 'Processing', date: '15/10/2020', icon: 'pi pi-cog', color: '#673AB7' },
-    { status: 'Shipped', date: '15/10/2020', icon: 'pi pi-shopping-cart', color: '#FF9800' },
-    { status: 'Delivered', date: '16/10/2020', icon: 'pi pi-check', color: '#607D8B' }
+    { status: 'Ordered', date: '15/10/2020', icon: 'pi pi-shopping-cart', color: 'var(--pink-600)', image: 'game-controller.jpg' },
+    { status: 'Processing', date: '15/10/2020', icon: 'pi pi-cog', color: 'var(--pink-600)' },
+    { status: 'Shipped', date: '15/10/2020', icon: 'pi pi-shopping-cart', color: 'var(--pink-600)' },
+    { status: 'Delivered', date: '16/10/2020', icon: 'pi pi-check', color: 'var(--pink-600)' }
 ];
 
   constructor() { }
