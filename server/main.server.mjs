1396c601369e8549a18ae156a6dad8bb8fdfce1a
Dani L <danilopezf10@gmail.com>
2024-12-15 13:37:39 +0100

    Auto-generated commit

--- server/main.server.mjs
+++ server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-DXE3WRPX.mjs";import"./chunk-ENMK4XPL.mjs";import"./chunk-7YLNGGYM.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
+import{a}from"./chunk-22TRJQSP.mjs";import"./chunk-ENMK4XPL.mjs";import"./chunk-7YLNGGYM.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
